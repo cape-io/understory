f947be6ca2ff91c443d7d81cea011d05438d3242
Kai Curry <kai@cape.io>
2019-01-11 14:21:58 -0500

    overBranch indivdual file

---
+++ src/overBranch.js
@@ -0,0 +1,14 @@
+import {
+  cond, identity, stubTrue,
+} from 'lodash/fp'
+
+/**
+ * Passes argument to boolCheck function. If true sends same argument to `getTrue` function.
+ * @param  {Function} boolCheck Function that check if value is true.
+ * @param  {Function} getTrue Get the value when true.
+ * @param  {Function} getTrue Optional. Get value when false.
+ * @return {any} Result of getTrue or getFalse.
+ */
+export default function overBranch(boolCheck, getTrue, getFalse = identity) {
+  return cond([[boolCheck, getTrue], [stubTrue, getFalse]])
+}
