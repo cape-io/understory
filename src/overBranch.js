3e1d018daa4231f621600077cd093bb5367bf97b
Kai Curry <kai@cape.io>
2019-01-15 09:31:40 -0500

    most basic example of overBranch

--- src/overBranch.js
+++ src/overBranch.js
@@ -8,6 +8,7 @@ import {
  * @param  {Function} getTrue Get the value when true.
  * @param  {Function} getTrue Optional. Get value when false.
  * @return {any} Result of getTrue or getFalse.
+ * @example overBranch(boolCheck, getTrue)
  */
 export default function overBranch(boolCheck, getTrue, getFalse = identity) {
   return cond([[boolCheck, getTrue], [stubTrue, getFalse]])
