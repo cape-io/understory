2d3ef768fac3804f0f5d0bb797fbcf59e4864a9c
Kai Curry <kai@cape.io>
2021-09-03 19:02:41 +0100

    move main to root

---
+++ index.js
@@ -0,0 +1,4 @@
+export { default as branch } from './src/branch.js'
+export * from './src/overBranch.js'
+export * from './src/utils.js'
+export * from './src/promise.js'
--- src/index.js
+++
@@ -1,4 +0,0 @@
-export { default as branch } from './branch.js'
-export * from './overBranch.js'
-export * from './utils.js'
-export * from './promise.js'
